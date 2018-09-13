d46284fc4394c469b34ac031c95bb3c3a708f038
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-13 11:40:34 +0200

    return component from lesson

--- openclassroom/MoviesAndMe/Components/Search.js
+++ openclassroom/MoviesAndMe/Components/Search.js
@@ -4,10 +4,9 @@ import {StyleSheet, View, Button, TextInput } from 'react-native'
 class Search extends React.Component {
     render() {
         return (
-            <View style={{ flex:1, backgroundColor: 'yellow', justifyContent: 'center', alignItems: 'center'}}>
-             <View style={{height:50, width: 50, backgroundColor: 'red' }}></View>
-             <View style={{height:50, width: 50, backgroundColor: 'green' }}> </View>
-             <View style={{height:50, width: 50, backgroundColor: 'blue' }}> </View>
+            <View style={{ marginTop: 20 }}>
+            <TextInput style={{ marginLeft:5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, height: 50, paddingLeft: 5 }} placeholder="Titre du film"></TextInput>
+            <Button style={{ height: 50 }} title="Rechercher" onPress={() => {}} />
             </View>
         )
     }
