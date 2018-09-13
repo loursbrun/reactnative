474d86304103dd8556e05a904f3689b9e4911f79
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-13 09:10:29 +0200

    add new component

---
+++ openclassroom/MoviesAndMe/Components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View, Button, TextInput } from 'react-native'
+
+class Search extends React.Component {
+    render() {
+        return (
+            <View>
+                <TextInput placeholder="Titre du film"></TextInput>
+                <Button title="Rechercher" />
+            </View>
+        )
+    }
+}
+
+export default Search
