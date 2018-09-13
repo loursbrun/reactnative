290a99b1912397ab708a14bfa7511a9140372fb2
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-13 13:06:09 +0200

    training flexbox

--- openclassroom/MoviesAndMe/Components/FilmItem.js
+++ openclassroom/MoviesAndMe/Components/FilmItem.js
@@ -7,7 +7,18 @@ class FilmItem extends React.Component {
   render() {
     return (
       <View style={styles.main_container}>
-        <Text style={styles.title_text}>Titre du film</Text>
+        <View name="left_container" style={{ backgroundColor: 'green', flex:1}}>
+        <View name="image_container" style={{ backgroundColor: 'blue', flex:1, margin:10}}>
+        </View>
+        </View>
+        <View name="right_container" style={{ backgroundColor: 'red', flex:2, margin: 10}}>
+             <View name="title_top_container" style={{ backgroundColor: 'grey', flex:1, flexDirection: 'row'}}>
+                <View name="title_left_container" style={{ backgroundColor: 'yellow', flex:2}}></View>
+                <View name="note_right_container" style={{ backgroundColor: 'orange', flex:1}}></View>
+             </View>
+             <View name="description_container" style={{ backgroundColor: 'pink', flex:3}}></View>
+             <View name="date_bottom_container" style={{ backgroundColor: 'brown', flex:1}}></View>
+        </View>
       </View>
     )
   }
@@ -15,7 +26,10 @@ class FilmItem extends React.Component {
 
 const styles = StyleSheet.create({
   main_container: {
-    height: 190
+    height: 200,
+    backgroundColor: 'grey',
+    marginBottom: 20,
+    flexDirection: 'row'
   },
   title_text: {
     
