39052611d0485d485a803820ebe94bb227d79cfa
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-13 11:45:47 +0200

    add styling

--- openclassroom/MoviesAndMe/Components/Search.js
+++ openclassroom/MoviesAndMe/Components/Search.js
@@ -4,8 +4,8 @@ import {StyleSheet, View, Button, TextInput } from 'react-native'
 class Search extends React.Component {
     render() {
         return (
-            <View style={{ marginTop: 20 }}>
-            <TextInput style={{ marginLeft:5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, height: 50, paddingLeft: 5 }} placeholder="Titre du film"></TextInput>
+            <View style={styles.main_container}>
+            <TextInput style={styles.textinput} placeholder="Titre du film"></TextInput>
             <Button style={{ height: 50 }} title="Rechercher" onPress={() => {}} />
             </View>
         )
@@ -14,6 +14,10 @@ class Search extends React.Component {
 
 
 const styles = StyleSheet.create( {
+    main_container: {
+        marginTop: 20, 
+        flex: 1
+    },
     textinput: {
         marginLeft:5, 
         marginRight: 5, 
