cacb77c3c55f427b4a87e64d95cd7bdd4bd88d1e
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-13 10:20:45 +0200

    add style to component direct in components

--- openclassroom/MoviesAndMe/Components/Search.js
+++ openclassroom/MoviesAndMe/Components/Search.js
@@ -4,9 +4,9 @@ import {View, Button, TextInput } from 'react-native'
 class Search extends React.Component {
     render() {
         return (
-            <View>
-                <TextInput placeholder="Titre du film"></TextInput>
-                <Button title="Rechercher" />
+            <View style={{ marginTop: 20 }}>
+                <TextInput style={{ marginLeft:5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, height: 50, paddingLeft: 5 }} placeholder="Titre du film"></TextInput>
+                <Button style={{ height: 50 }} title="Rechercher" onPress={() => {}} />
             </View>
         )
     }
