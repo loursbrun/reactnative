a406f8d69961c70b9b64aa1c20dea252db032505
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-13 12:01:56 +0200

    add simple flatlist

--- openclassroom/MoviesAndMe/Components/Search.js
+++ openclassroom/MoviesAndMe/Components/Search.js
@@ -1,30 +1,34 @@
 import React from 'react'
-import {StyleSheet, View, Button, TextInput } from 'react-native'
+import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
 
 class Search extends React.Component {
     render() {
         return (
             <View style={styles.main_container}>
-            <TextInput style={styles.textinput} placeholder="Titre du film"></TextInput>
-            <Button style={{ height: 50 }} title="Rechercher" onPress={() => {}} />
+                <TextInput style={styles.textinput} placeholder="Titre du film"></TextInput>
+                <Button style={{ height: 50 }} title="Rechercher" onPress={() => { }} />
+                <FlatList
+                    data={[{ key: 'a' }, { key: 'b' }]}
+                    renderItem={({ item }) => <Text>{item.key}</Text>}
+                />
             </View>
         )
     }
 }
 
 
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
     main_container: {
-        marginTop: 20, 
+        marginTop: 20,
         flex: 1
     },
     textinput: {
-        marginLeft:5, 
-        marginRight: 5, 
-        height: 50, 
-        borderColor: '#000000', 
-        borderWidth: 1, 
-        height: 50, 
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        height: 50,
         paddingLeft: 5
     }
 })
