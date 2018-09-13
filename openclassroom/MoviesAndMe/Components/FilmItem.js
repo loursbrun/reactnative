50e7c27127592e148219703214df562a07c46336
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-13 13:31:07 +0200

    play with flexbox

--- openclassroom/MoviesAndMe/Components/FilmItem.js
+++ openclassroom/MoviesAndMe/Components/FilmItem.js
@@ -13,11 +13,11 @@ class FilmItem extends React.Component {
         </View>
         <View name="right_container" style={{ backgroundColor: 'red', flex:2, margin: 10}}>
              <View name="title_top_container" style={{ backgroundColor: 'grey', flex:1, flexDirection: 'row'}}>
-                <View name="title_left_container" style={{ backgroundColor: 'yellow', flex:2}}></View>
-                <View name="note_right_container" style={{ backgroundColor: 'orange', flex:1}}></View>
+                <View name="title_left_container" style={{ backgroundColor: 'yellow', flex:2}}><Text style={{ fontSize: 20, fontWeight: '600'}}>Titre du film</Text></View>
+                <View name="note_right_container" style={{ backgroundColor: 'orange', flex:1}}><Text style={{ fontSize: 30, fontWeight: '600', textAlign: 'right'}}>Vote</Text></View>
              </View>
-             <View name="description_container" style={{ backgroundColor: 'pink', flex:3}}></View>
-             <View name="date_bottom_container" style={{ backgroundColor: 'brown', flex:1}}></View>
+             <View name="description_container" style={{ backgroundColor: 'pink', flex:3}}><Text style={{ fontStyle:'italic'}}>Description</Text></View>
+             <View name="date_bottom_container" style={{ backgroundColor: 'brown', flex:1}}><Text style={{ fontSize: 14, fontWeight: '600', flexDirection:'column',  backgroundColor:'red', position:'absolute', bottom: 0, right: 0 }}>Sortie le 00/00/00</Text></View>
         </View>
       </View>
     )
