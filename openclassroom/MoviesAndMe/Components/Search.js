fad2610bb82ff5349f10a59ea8071f5b7ca9ba0a
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-14 15:55:34 +0200

    pass id from navigation params

--- openclassroom/MoviesAndMe/Components/Search.js
+++ openclassroom/MoviesAndMe/Components/Search.js
@@ -18,7 +18,7 @@ class Search extends React.Component {
     }
 
     _displayDetailForFilm = (idFilm) => {
-        this.props.navigation.navigate("FilmDetail")
+        this.props.navigation.navigate("FilmDetail", { idFilm: idFilm })
     } 
 
     _loadFilms() {
