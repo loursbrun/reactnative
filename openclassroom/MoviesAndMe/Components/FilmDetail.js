896e514a0b3d239254cf54c0a11c7413a7a36236
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-14 17:06:02 +0200

    show detail film view

--- openclassroom/MoviesAndMe/Components/FilmDetail.js
+++ openclassroom/MoviesAndMe/Components/FilmDetail.js
@@ -1,21 +1,74 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView } from 'react-native'
+import { getFilmDetailFromApi } from '../API/TMDBAApi'
 
 class FilmDetail extends React.Component {
-  render() {
-      const idFilm = this.props.navigation.state.params.idFilm
-    return (
-      <View style={styles.main_container}>
-        <Text>Détail du film : {idFilm}</Text>
-      </View>
-    )
-  }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            film: undefined,
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+            this.setState({
+                film: data,
+                isLoading: false
+            })
+        })
+    }
+
+    _displayFilm() {
+        const film = this.state.film
+        if (film != undefined) {
+            return (
+                <ScrollView style={styles.scrollview_container}>
+                    <Text>{film.title}</Text>
+                </ScrollView>
+            )
+        }
+    }
+
+    _dispalyLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
+    } 
+
+    render() {
+        const idFilm = this.props.navigation.state.params.idFilm
+        return (
+            <View style={styles.main_container}>
+                {this._displayFilm()}
+                {this._dispalyLoading()}
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  main_container: {
-    flex: 1,
-  }
+    main_container: {
+        flex: 1,
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    scrollview_container: {
+        flex: 1
+    }
 })
 
 export default FilmDetail
