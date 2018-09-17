a1b682f8f9aeabac617d19736e751e0dd7b8aa2c
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-17 17:01:08 +0200

    update correction exercice

--- openclassroom/MoviesAndMe/Components/FilmItem.js
+++ openclassroom/MoviesAndMe/Components/FilmItem.js
@@ -3,29 +3,27 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 import { getImageFromApi } from '../API/TMDBApi'
-import { connect } from 'react-redux'
-
 
 class FilmItem extends React.Component {
-  
 
   _displayFavoriteImage() {
-    console.log("******" + this.props.isFilmFavorite)
-    if (this.props.isFilmFavorite) 
-    return (
-      <Image style={styles.icon_fav} source={require('../Images/ic_favorite.png')} />
-    )
-    
-}
-
+    if (this.props.isFilmFavorite) {
+      // Si la props isFilmFavorite vaut true, on affiche le 🖤
+      return (
+        <Image
+          style={styles.favorite_image}
+          source={require('../Images/ic_favorite.png')}
+        />
+      )
+    }
+  }
 
   render() {
-    
     const { film, displayDetailForFilm } = this.props
     return (
-      <TouchableOpacity 
-      onPress={() => displayDetailForFilm(film.id)}
-      style={styles.main_container}>
+      <TouchableOpacity
+        style={styles.main_container}
+        onPress={() => displayDetailForFilm(film.id)}>
         <Image
           style={styles.image}
           source={{uri: getImageFromApi(film.poster_path)}}
@@ -38,10 +36,9 @@ class FilmItem extends React.Component {
           </View>
           <View style={styles.description_container}>
             <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
-            {/* La propriété numberOfLines permet de couper un texte si celui-ci est trop long, il suffit de définir un nombre maximum de ligne */}
           </View>
           <View style={styles.date_container}>
-            <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
+            <Text style={styles.date_text}>Sorti le 13/12/2017</Text>
           </View>
         </View>
       </TouchableOpacity>
@@ -57,8 +54,7 @@ const styles = StyleSheet.create({
   image: {
     width: 120,
     height: 180,
-    margin: 5,
-    backgroundColor: 'gray'
+    margin: 5
   },
   content_container: {
     flex: 1,
@@ -67,11 +63,6 @@ const styles = StyleSheet.create({
   header_container: {
     flex: 3,
     flexDirection: 'row'
-  },
-  icon_fav: {
-    width: 30,
-    height: 30,
-    marginRight: 2
   },
   title_text: {
     fontWeight: 'bold',
@@ -98,16 +89,12 @@ const styles = StyleSheet.create({
   date_text: {
     textAlign: 'right',
     fontSize: 14
+  },
+  favorite_image: {
+    width: 25,
+    height: 25,
+    marginRight: 5
   }
 })
 
-//export default FilmItem
-
-
-const mapStateToProps = (state) => {
-  return {
-      favoritesFilm: state.favoritesFilm
-  }
-}
-export default connect(mapStateToProps)(FilmItem)
-
+export default FilmItem
--- openclassroom/MoviesAndMe/Components/Search.js
+++ openclassroom/MoviesAndMe/Components/Search.js
@@ -1,122 +1,129 @@
+// Components/Search.js
+
 import React from 'react'
-import { StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator } from 'react-native'
-import films from '../Helpers/filmsData'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 import { connect } from 'react-redux'
 
-
 class Search extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            films: [],
+  constructor(props) {
+    super(props)
+    this.searchedText = ""
+    this.page = 0
+    this.totalPages = 0
+    this.state = {
+      films: [],
+      isLoading: false
+    }
+  }
+
+  _loadFilms() {
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true })
+      getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+          this.page = data.page
+          this.totalPages = data.total_pages
+          this.setState({
+            films: [ ...this.state.films, ...data.results ],
             isLoading: false
-        }
-        this.page = 0
-        this.totalPages = 0
-        this.searchedText = ""
-    }
-
-    _displayDetailForFilm = (idFilm) => {
-        this.props.navigation.navigate("FilmDetail", { idFilm: idFilm })
-    } 
-
-    _loadFilms() {
-        if (this.searchedText.length > 0) {
-          this.setState({ isLoading: true })
-          getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
-              this.page = data.page
-              this.totalPages = data.total_pages
-              this.setState({
-                films: [ ...this.state.films, ...data.results ],
-                isLoading: false
-              })
           })
-        }
-      }
-    
-    _dispalyLoading() {
-        if (this.state.isLoading) {
-            return (
-                <View style={styles.loading_container}>
-                    <ActivityIndicator size='large' />
-                </View>
-            )
-        }
+      })
     }
+  }
 
-    _searchTextInputChanged(text) {
-        this.searchedText = text
+  _searchTextInputChanged(text) {
+    this.searchedText = text
+  }
+
+  _searchFilms() {
+    this.page = 0
+    this.totalPages = 0
+    this.setState({
+      films: [],
+    }, () => {
+        this._loadFilms()
+    })
+  }
+
+  _displayDetailForFilm = (idFilm) => {
+    console.log("Display film with id " + idFilm)
+    this.props.navigation.navigate("FilmDetail", { idFilm: idFilm })
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
     }
+  }
 
-    _searchFilms() {
-        this.page = 0
-        this.totalPages = 0
-        this.setState({
-            films: []
-        }, () => {
-            this._loadFilms()
-        })
-    } 
-
-    render() {
-        console.log(this.state.isLoading);
-        return (
-            <View style={styles.main_container}>
-                <TextInput onSubmitEditing={() => this._searchFilms()} onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textinput} placeholder="Titre du film"></TextInput>
-                <Button style={{ height: 50 }} title="Rechercher" onPress={() => this._searchFilms()} />
-                <FlatList
-                    data={this.state.films}
-                    keyExtractor={(item) => item.id.toString()}
-                    onEndReachedThreshold={0.5}
-                    onEndReached={() => {
-                        if ((this.state.films.length > 0 && this.page < this.totalPages)) {
-                            this._loadFilms()
-                        }
-                    }}
-                    renderItem={({ item }) => 
-                    <FilmItem 
-                    film={item} 
-                     // Ajout d'une props isFilmFavorite pour indiquer à l'item d'afficher un 🖤 ou non
-                    isFilmFavorite={(this.props.favoritesFilm.findIndex(film => film.id === item.id) !== -1) ? true : false}
-                    displayDetailForFilm={this._displayDetailForFilm} 
-                    />}
-                />
-                {this._dispalyLoading()}
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._searchFilms()}
+        />
+        <Button style={{ height: 50 }} title='Rechercher' onPress={() => this._searchFilms()}/>
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) =>
+            <FilmItem
+              film={item}
+              // Ajout d'une props isFilmFavorite pour indiquer à l'item d'afficher un 🖤 ou non
+              isFilmFavorite={(this.props.favoritesFilm.findIndex(film => film.id === item.id) !== -1) ? true : false}
+              displayDetailForFilm={this._displayDetailForFilm}
+            />
+          }
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+              if (this.state.films.length > 0 && this.page < this.totalPages) { // On vérifie également qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+                 this._loadFilms()
+              }
+          }}
+        />
+        {this._displayLoading()}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        flex: 1
-    },
-    textinput: {
-        marginLeft: 5,
-        marginRight: 5,
-        height: 50,
-        borderColor: '#000000',
-        borderWidth: 1,
-        height: 50,
-        paddingLeft: 5
-    },
-    loading_container: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 100,
-        bottom: 0,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  main_container: {
+    flex: 1
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
 
-const mapStateToProps = (state) => {
-    return {
-        favoritesFilm: state.favoritesFilm
-    }
+// On connecte le store Redux, ainsi que les films favoris du state de notre application, à notre component Search
+const mapStateToProps = state => {
+  return {
+    favoritesFilm: state.favoritesFilm
+  }
 }
+
 export default connect(mapStateToProps)(Search)
