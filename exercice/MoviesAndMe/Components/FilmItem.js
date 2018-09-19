75afb8c817a40763164967a9d1f148160ceb6fa0
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-19 18:24:53 +0200

    add different view between Seens and News

--- exercice/MoviesAndMe/Components/FilmItem.js
+++ exercice/MoviesAndMe/Components/FilmItem.js
@@ -24,6 +24,7 @@ class FilmItem extends React.Component {
   _displayFavoritView({ film, displayDetailForFilm }) {
     console.log("********")
     console.log(this.props.navigation.state.routeName)
+    if(this.props.navigation.state.routeName === 'News') {
     return (
       <TouchableOpacity
           style={styles.main_container}
@@ -46,7 +47,31 @@ class FilmItem extends React.Component {
             </View>
           </View>
         </TouchableOpacity>
-    )
+    ) } 
+    else if (this.props.navigation.state.routeName === 'Seens') {
+      return (
+        <TouchableOpacity
+            style={styles.main_container}
+            onPress={() => displayDetailForFilm(film.id)}>
+            <Image
+              style={styles.image}
+              source={{uri: getImageFromApi(film.poster_path)}}
+            />
+            <View style={styles.content_container}>
+              <View style={styles.header_container}>
+                {this._displayFavoriteImage()}
+                <Text style={styles.title_text}>{film.title}</Text>
+               
+              </View>
+              <View style={styles.description_container}>
+                <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
+              </View>
+              <View style={styles.date_container}>
+                <Text style={styles.date_text}>Sorti le 13/12/2017</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+      ) } 
   }
 
 
