7d0efa8e44d4b7fc1348791cdb897cc75ddf6426
mkinaxia <mkinaxia@Mac-mini-de-mkinaxia.local>
2018-09-19 09:17:35 +0200

    Add persistance for reduxe

--- openclassroom/MoviesAndMe/App.js
+++ openclassroom/MoviesAndMe/App.js
@@ -2,7 +2,9 @@
 import React from 'react'
 import Navigation from './Navigation/Navigation'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/es/integration/react'
 import Store from './Store/configureStore'
+import { persistStore } from 'redux-persist'
 
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
@@ -10,9 +12,12 @@ YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTIm
 
 export default class App extends React.Component {
   render() {
+    let persistor = persistStore(Store)
     return (
       <Provider store={Store}>
-        <Navigation/>
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
       </Provider>
     )
   }
